9f24dd1d348f2737291617b102a4fae1ab73a83e
tianering <Tianering@yeah.net>
2021-07-13 15:47:11 +0800

    首次提交

---
+++ build/CMakeFiles/rectify.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rectify.dir/main.cpp.o"
+  "rectify"
+  "rectify.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rectify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
